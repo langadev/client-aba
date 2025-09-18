5aee712a2d968e455f16563b1e34c65e96e8afec
root <root@srv1011014.hstgr.cloud>
2025-09-18 10:47:56 +0000

    Env files

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance with a base URL and default headers
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
